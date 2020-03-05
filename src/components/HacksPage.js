ad705f10aa154367e1765e8688cb408190c7f3ea
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-06 10:51:21 +1100

    added some basic styling

--- src/components/HacksPage.js
+++ src/components/HacksPage.js
@@ -2,13 +2,6 @@ import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
-import InboxIcon from "@material-ui/icons/Inbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles(theme => ({
@@ -16,28 +9,43 @@ const useStyles = makeStyles(theme => ({
     width: "100%",
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper
+  },
+  paper: {
+    textAlign: "center",
+    color: theme.palette.text.primary
   }
 }));
-
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
 
 const Hacks = () => {
   const classes = useStyles();
   const [hacks, setHacks] = useState([
-    { name: "test1", title: "eggg", description: "eggs?" },
-    { name: "test2", title: "egg2", description: "more eggs" },
-    { name: "test3", title: "egg heaven", description: "get more eggs" }
+    {
+      name: "World Peace",
+      title: "Egg day",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur aliquet quam id dui posuere blandit."
+    },
+    {
+      name: "Nodejs day",
+      title: "Hooray",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur aliquet quam id dui posuere blandit."
+    },
+    {
+      name: "MAC day",
+      title: "$$$",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur aliquet quam id dui posuere blandit."
+    }
   ]);
   const renderHacks = () => {
     return hacks.map(hack => {
       return (
         <Grid item xs={12}>
-          <Paper key={hack.title}>
-            <h2>{hack.name}</h2>
-            <h3>{hack.title}</h3>
-            <h4>{hack.description}</h4>
+          <Paper className={classes.paper} key={hack.title}>
+            <h2>Name: {hack.name}</h2>
+            <h3>Title: {hack.title}</h3>
+            <h4>Description: {hack.description}</h4>
           </Paper>
         </Grid>
       );
