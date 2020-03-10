06502350081a1845db493cac2bd9e531c622e28d
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-10 12:26:32 +1100

    Page header added with login and register buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CreateHack from "./components/create-hack";
 import HacksPage from "./components/HacksPage";
 import HackDetails from "./components/hack-details";
+import Header from './components/header'
+import './css/main.css'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Switch>
-        <Route path="/" component={<h1>This is root</h1>}/>
+        {/* <Route exact path="/" component={<h1>This is root</h1>}/> */}
         <Route path="/createHack" component={CreateHack}/>
         <Route path="/hacks" component={HacksPage}/>
         <Route path="/hack/:id" component={HackDetails}/>
