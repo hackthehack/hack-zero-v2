378d9bab32ffecbb8c6675d78d2a58521baa103f
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-10 13:46:42 +1100

    Added Register page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CreateHack from "./components/create-hack";
 import HacksPage from "./components/HacksPage";
 import HackDetails from "./components/hack-details";
+import Register from './components/register'
 import Header from './components/header'
 import './css/main.css'
 
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Switch>
         {/* <Route exact path="/" component={<h1>This is root</h1>}/> */}
+        <Route path="/register" component={Register}/>
         <Route path="/createHack" component={CreateHack}/>
         <Route path="/hacks" component={HacksPage}/>
         <Route path="/hack/:id" component={HackDetails}/>
