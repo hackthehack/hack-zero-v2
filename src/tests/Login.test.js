a0e11da22763baa16b4c7a189a0ace2b375e589b
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-11 16:54:52 +1100

    finished the simulate submit event test

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Login } from "../components/Login";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 test("If <Login/> Component displays Login headig", () => {
   const { getByTestId } = render(<Login />);
@@ -12,4 +12,12 @@ test("If <Login/> Component displays a login button", () => {
   const { getByTestId } = render(<Login />);
   const button = getByTestId("login-button");
   expect(button).toBeInTheDocument();
+});
+
+test("If the <Login/> component calls the login function when submit button is pressed", () => {
+  const mockLogin = jest.fn();
+  const { getByTestId } = render(<Login auth={mockLogin} />);
+  const button = getByTestId("login-button");
+  fireEvent.click(button);
+  expect(mockLogin).toHaveBeenCalled();
 });
