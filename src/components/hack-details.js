2c18028ac9608a6d566c534d849d6e9eb012ad90
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-12 14:11:27 +1100

    Join hack button working with hard coded data, waitind for redux

--- src/components/hack-details.js
+++ src/components/hack-details.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 // UI imports
-import { Grid, Typography, Paper, Button, Chip } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  Chip,
+  CircularProgress
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ErrorOutlineOutlinedIcon from "@material-ui/icons/ErrorOutlineOutlined";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,6 +22,12 @@ const useStyles = makeStyles(theme => ({
   },
   rightFeild: {
     textAlign: "right"
+  },
+  loading: {
+    marginTop: theme.spacing(10)
+  },
+  chipMArgin: {
+    marginRight: theme.spacing(1)
   }
 }));
 
@@ -22,79 +37,121 @@ function HackDetails(props) {
   const [data] = useState();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [team, setTeam] = useState();
   const [goal, setGoal] = useState("");
 
   useEffect(() => {
     //console.log(props.match.params);
+
     if (props.match !== undefined) {
-      Axios.get(process.env.REACT_APP_API_URL+"hackdetail/" + props.match.params.id)
+      Axios.get(
+        process.env.REACT_APP_API_URL + "hackdetail/" + props.match.params.id
+      )
         .then(res => {
           setTitle(res.data.title);
           setDescription(res.data.description);
           setGoal(res.data.goal);
+          setTeam(res.data.team);
         })
-        .then(() => {
-          console.log(data);
-        });
-    } else {
-      setTitle("Hack-Zero");
-      setDescription(
-        "This is where the description/idea for the hack will be displayed when there is one. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus in tortor a molestie. Vestibulum congue, eros et ultricies vehicula, sapien est sollicitudin risus, ut volutpat augue tellus in neque. Aliquam erat volutpat."
-      );
-      setGoal("To win and get that dosh but also the experience");
     }
   }, [data, props.match]);
 
-  return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="stretch"
-      alignContent="center"
-    >
-      <Paper className={classes.root}>
-        <Grid
-          container
-          justify="space-between"
-          alignItems="center"
-          alignContent="center"
-          spacing={1}
-        >
+  const joinHack = event => {
+    const object = {
+      hackId: "5e6999fb5c13b5d80505cde4",
+      userId: "5e683ccc10c5d40008a40e56"
+    };
+    Axios.post(process.env.REACT_APP_API_URL + "joinhack", object).then(res => {
+      setTeam(res.data.team);
+    });
+  };
+
+  const showTeam = () => {
+    if (team !== undefined) {
+      return (
+        <Grid item xs={12}>
+          <Typography variant="h6">Team:</Typography>
           <Grid item xs={10}>
-            <Chip
-              icon={<ErrorOutlineOutlinedIcon />}
-              label="status"
-              color="secondary"
-            />
-          </Grid>
-          <Grid item xs={2} className={classes.rightFeild}>
-            <Button variant="contained" color="primary">
-              Join Hack
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h4">{title}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">Idea:</Typography>
-          </Grid>
-          <Grid item xs={10}>
-            <Typography variant="body1">{description}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">Goal:</Typography>
-          </Grid>
-          <Grid item xs={10}>
-            <Typography variant="body1">{goal}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">Team:</Typography>
+            {team.map(id => {
+              return (
+                <Chip
+                  key={id}
+                  className={classes.chipMArgin}
+                  icon={<AccountCircleIcon />}
+                  label={id}
+                  color="primary"
+                />
+              );
+            })}
           </Grid>
         </Grid>
-      </Paper>
-    </Grid>
-  );
+      );
+    }
+  };
+
+  if (title !== "") {
+    return (
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="stretch"
+        alignContent="center"
+      >
+        <Paper className={classes.root}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="center"
+            alignContent="center"
+            spacing={1}
+          >
+            <Grid item xs={9}>
+              <Chip
+                icon={<ErrorOutlineOutlinedIcon />}
+                label="status"
+                color="secondary"
+              />
+            </Grid>
+            <Grid item xs={3} className={classes.rightFeild}>
+              <Button variant="contained" color="primary" onClick={joinHack}>
+                Join Hack
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h4">{title}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Idea:</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant="body1">{description}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Goal:</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant="body1">{goal}</Typography>
+            </Grid>
+            {showTeam()}
+          </Grid>
+        </Paper>
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="stretch"
+        alignContent="center"
+        className={classes.loading}
+      >
+        <CircularProgress />
+      </Grid>
+    );
+  }
 }
 
 export default HackDetails;
