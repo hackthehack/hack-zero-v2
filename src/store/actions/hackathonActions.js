d44e2e15720210c609a67882215f51805d9f513c
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-17 10:09:06 +1100

    placed contentful api address to .env file and also move all contentful id there as well

--- src/store/actions/hackathonActions.js
+++ src/store/actions/hackathonActions.js
@@ -1,6 +1,6 @@
 import * as ActionType from "./index";
 import axios from "axios";
-const contentfulUrl = "https://cdn.contentful.com";
+//const contentfulUrl = "https://cdn.contentful.com";
 
 const getContentOkay = content => ({
   type: ActionType.FETCH_HACK_A_THON,
@@ -11,7 +11,7 @@ export const getHackathonContent = () => {
   return async (dispatch, getState) => {
     //console.log(process.env.REACT_APP_CONTENTFUL_KEY);
     let result = await axios.get(
-      `${contentfulUrl}/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_KEY}&content_type=hackEvent`
+      `${process.env.REACT_APP_CONTENTFUL_API}/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.REACT_APP_CONTENTFUL_KEY}&content_type=hackEvent`
     );
     dispatch(getContentOkay(result.data.items));
     //console.log(result.data);
