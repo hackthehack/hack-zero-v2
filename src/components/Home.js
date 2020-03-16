71816d05f9ff38861f09dcbcdcc46019eb2dcec0
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-17 10:47:53 +1100

    our sourced all cards into One

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,74 +34,25 @@ export const Home = ({
   price
 }) => {
   const classes = useStyles();
-  const scheduleCard = (
-    <Card data-testid="schedule-card" style={{ width: "100%" }}>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Schedule:
-        </Typography>
 
-        <Typography variant="body2" component="p">
-          From: {from}
-        </Typography>
-        <Typography variant="body2" component="p">
-          To: {to}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-  const priceCard = (
-    <Card data-testid="price-card" style={{ width: "100%" }}>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Price:
-        </Typography>
+  const FixedCard = ({ id, title, values }) => {
+    return (
+      <Card data-testid={`${id}-card`} style={{ width: "100%" }}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            {title}
+          </Typography>
 
-        <Typography variant="body2" component="p">
-          {price}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-  const themeCard = (
-    <Card data-testid="theme-card" style={{ width: "100%" }}>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Theme:
-        </Typography>
+          {values.map((value, index) => (
+            <Typography key={index} variant="body2" component="p">
+              {value}
+            </Typography>
+          ))}
+        </CardContent>
+      </Card>
+    );
+  };
 
-        <Typography variant="body2" component="p">
-          {theme}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-  const statusCard = (
-    <Card data-testid="status-card" style={{ width: "100%" }}>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Status:
-        </Typography>
-
-        <Typography variant="body2" component="p">
-          {status}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-  const informationCard = (
-    <Card data-testid="information-card">
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Information:
-        </Typography>
-
-        <Typography variant="body2" component="p">
-          {information}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
   useEffect(() => {
     dispatch(getHackathonContent());
   }, [dispatch]);
@@ -120,19 +71,27 @@ export const Home = ({
         </Typography>
         <Grid container spacing={3} alignItems="stretch">
           <Grid style={{ display: "flex" }} item xs={12} sm={6}>
-            {priceCard}
+            <FixedCard id="price" title="Price" values={[price]} />
           </Grid>
           <Grid style={{ display: "flex" }} item xs={12} sm={6}>
-            {statusCard}
+            <FixedCard id="status" title="Status" values={[status]} />
           </Grid>
           <Grid style={{ display: "flex" }} item xs={12} sm={6}>
-            {themeCard}
+            <FixedCard id="theme" title="Theme" values={[theme]} />
           </Grid>
           <Grid style={{ display: "flex" }} item xs={12} sm={6}>
-            {scheduleCard}
+            <FixedCard
+              id="schedule"
+              title="Schedule"
+              values={[`From: ${from}`, `To: ${to}`]}
+            />
           </Grid>
           <Grid item xs={12}>
-            {informationCard}
+            <FixedCard
+              id="information"
+              title="Information"
+              values={[information]}
+            />
           </Grid>
         </Grid>
       </Container>
