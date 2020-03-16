654cacce0fcae59add94d5def96a8b6ac519a5e0
HuangStanley050 <infamous_godhand@yahoo.com>
2020-03-16 16:46:07 +1100

    removing a duplicate Login component on top

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HackDetails from "./components/hack-details";
 import Login from "./components/Login";
 import Register from "./components/register";
 import Header from "./components/header";
-import Login from "./components/Login"
+
 import Home from "./components/Home";
 import "./css/main.css";
 
