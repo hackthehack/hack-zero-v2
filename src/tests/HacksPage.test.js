2b0bb964c2a2400624cc122a0fcb8fd2e097b046
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-24 16:44:08 +1100

    fixed one failed test on HacksPage.test

--- src/tests/HacksPage.test.js
+++ src/tests/HacksPage.test.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Hacks from "../components/HacksPage";
-
-
+import { Hacks } from "../components/HacksPage";
 
 test("If a container exists for displayng the hacks", () => {
-  const { getByTestId } = render( <Hacks/>);
-  const grid = getByTestId('main-container')
+  const { getByTestId } = render(<Hacks dispatch={() => "nothing"} />);
+  const grid = getByTestId("main-container");
   expect(grid).toBeInTheDocument();
 });
-
