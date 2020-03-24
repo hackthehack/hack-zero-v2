8eb2fcb962525b5f2e93edce4cb89ca36878b766
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-24 13:27:40 +1100

    start

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
+
 import CardContent from "@material-ui/core/CardContent";
 import { connect } from "react-redux";
 import { getHackathonContent } from "../store/actions/hackathonActions";
