bc44c4bfdc52205c0211f4e05ab1165ac0b79c1d
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-24 16:49:31 +1100

    fixed hack-details.test

--- src/tests/hack-details.test.js
+++ src/tests/hack-details.test.js
@@ -1,18 +1,17 @@
 import React from "react";
 
 import { render } from "@testing-library/react";
-import HackDetails from "../components/hack-details";
+import { HackDetails } from "../components/hack-details";
 import { Provider } from "react-redux";
 import store from "../setupStore";
 
-
 test("If a container exists for displayng the hacks", () => {
-  const { getByTestId } = render(<Provider store={store}>
+  const { getByTestId } = render(
     <HackDetails
+      dispatch={() => "nothing"}
       match={{ params: { id: 1 }, isExact: true, path: "", url: "" }}
     />
-  </Provider>)
+  );
   const grid = getByTestId("main-container");
   expect(grid).toBeInTheDocument();
 });
-
