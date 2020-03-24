1fc55e4b32bda3af3b377daa11b4ca81376b7953
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-24 16:26:52 +1100

    Fixing failing tests

--- src/tests/HacksPage.test.js
+++ src/tests/HacksPage.test.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Hacks } from "../components/HacksPage";
+import Hacks from "../components/HacksPage";
+
+
 
 test("If a container exists for displayng the hacks", () => {
-  const { getByTestId } = render(<Hacks />);
+  const { getByTestId } = render( <Hacks/>);
   const grid = getByTestId('main-container')
   expect(grid).toBeInTheDocument();
 });
 
-//further testing need after front end is connected to the backend for fetching the number of hacks, for now, will just test if the components render three hacks that was hardcoded.
-
-// test("If the component displays three hardcoded hacks on the page", () => {
-//   const { getAllByTestId } = render(<HacksPage />);
-//   const element = getAllByTestId("hack-idea");
-//   expect(element.length).toBe(3);
-// });
--- src/tests/hack-details.test.js
+++ src/tests/hack-details.test.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import { HackDetails } from "../components/hack-details";
+
+import { render } from "@testing-library/react";
+import HackDetails from "../components/hack-details";
+import { Provider } from "react-redux";
+import store from "../setupStore";
+
 
 test("If a container exists for displayng the hacks", () => {
-  const { getByTestId } = render(<HackDetails />);
-  const grid = getByTestId('main-container')
+  const { getByTestId } = render(<Provider store={store}>
+    <HackDetails
+      match={{ params: { id: 1 }, isExact: true, path: "", url: "" }}
+    />
+  </Provider>)
+  const grid = getByTestId("main-container");
   expect(grid).toBeInTheDocument();
 });
 
