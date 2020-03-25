3b23e0a080c24b12a716317ea125072f1be04b44
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-26 10:09:51 +1100

    some issue writing new test for Home

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,70 +1,60 @@
 import React from "react";
 import { render, act } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import ReduxConnectedHome, { Home } from "../components/Home";
 // import {
 //   getHackathonContent,
 //   getContentOkay
 // } from "../store/actions/hackathonActions";
-// import configureStore from "redux-mock-store";
-
-//const mockStore = configureStore([]);
-
-// test("<Home/> component should have the right number of hacks that user is assigned to", () => {
-//   const mockDispatch = jest.fn();
-//   const mockAssignedHacks = [
-//     { title: "hack1", description: "test", _id: 1 },
-//     { title: "hack2", description: "test", _id: 2 },
-//     { title: "hack3", description: "test", _id: 3 }
-//   ];
-//   const { getAllByTestId } = render(
-//     <BrowserRouter>
-//       <Home
-//         dispatch={mockDispatch}
-//         assignedHacks={mockAssignedHacks}
-//         isAuth={true}
-//       />
-//     </BrowserRouter>
-//   );
-//   const assignedHacks = getAllByTestId("assignedHack");
-//   //sconsole.log(assignedHacks);
-//   expect(assignedHacks.length).toBe(mockAssignedHacks.length);
-// });
-
-// test("<Home/> component should fetch data to show assigned hacks if user is  logged in", async () => {
-//   const mockDispatch = jest.fn();
-//   await act(async () => {
-//     render(<Home dispatch={mockDispatch} assignedHacks={[]} isAuth={true} />);
-//   });
-//   //first call will be to fetch from CMS
-//   //2nd call will fetch the user assigned hacks
-//   expect(mockDispatch).toHaveBeenCalledTimes(2);
-// });
-//
-// test("<Home/> component should not fetch data to show assigned hacks if user is not logged in", async () => {
-//   const mockDispatch = jest.fn();
-//
-//   await act(async () => {
-//     render(<Home dispatch={mockDispatch} assignedHacks={[]} isAuth={false} />);
-//   });
-//   //it will always call to get data from Contentful CMS when component mounst
-//   //it won't call to fetch user hacks if user not loggedin
-//   expect(mockDispatch).toHaveBeenCalledTimes(1);
-// });
+import configureStore from "redux-mock-store";
+const mockStore = configureStore();
 
 test("<Home/> component heading should be rendered in the page", () => {
-  const dispatch = jest.fn();
+  const initialState = {
+    auth: {
+      isAuth: false
+    },
+    hack: {
+      items: {
+        title: "a test",
+        information: "a information",
+        status: "active",
+        theme: "a testing theme"
+      },
+      assignedHacks: []
+    }
+  };
+  const store = mockStore(initialState);
   const { getByTestId } = render(
-    <Home dispatch={dispatch} assignedHacks={[]} />
+    <Provider store={store}>
+      <ReduxConnectedHome />
+    </Provider>
   );
   const header = getByTestId("page-header");
   expect(header).toBeInTheDocument();
 });
 
 test("<Home/> component should have 5 cards, information, schedule, price, theme and status", () => {
-  const dispatch = jest.fn();
+  const initialState = {
+    auth: {
+      isAuth: false
+    },
+    hack: {
+      items: {
+        title: "a test",
+        information: "a information",
+        status: "active",
+        theme: "a testing theme"
+      },
+      assignedHacks: []
+    }
+  };
+  const store = mockStore(initialState);
   const { getByTestId } = render(
-    <Home dispatch={dispatch} assignedHacks={[]} />
+    <Provider store={store}>
+      <ReduxConnectedHome />
+    </Provider>
   );
   const schedule = getByTestId("schedule-card");
   const status = getByTestId("status-card");
