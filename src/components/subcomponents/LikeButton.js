bfccb31ed9a30c63e1c63c3e73d60a2290bab340
StanleyHuang <infamous_godhand@yahoo.com>
2020-04-02 11:19:52 +1100

    added change to Like button when user clicks

--- src/components/subcomponents/LikeButton.js
+++ src/components/subcomponents/LikeButton.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThumbUp } from "@material-ui/icons";
 import Button from "@material-ui/core/Button";
 
 const LikeButton = () => {
+  const [likeStatus, setLikeStatus] = useState(false);
+  const toggleLike = () => setLikeStatus(!likeStatus);
   return (
-    <Button variant="outlined" color="primary">
-      <ThumbUp style={{ color: "dodgerBlue", fontSize: "2rem" }} />
+    <Button variant="outlined" color="primary" onClick={toggleLike}>
+      <ThumbUp
+        style={{ color: likeStatus ? "dodgerBlue" : "red", fontSize: "2rem" }}
+      />
     </Button>
   );
 };
