4d94b17322cefc16ee62ac6efa0a8715469ede0e
Stanley <infamous_godhand@yahoo.com>
2020-04-14 09:55:20 +1000

    first successful test on authReducer

--- src/tests/reducers/authReducer.test.js
+++ src/tests/reducers/authReducer.test.js
@@ -1,2 +1,13 @@
 import * as ActionType from "../../store/actions";
 import authReducer from "../../store/reducers/authReducer";
+
+test("Auth reducer returns default initialState", () => {
+  const action = { type: "dummy_action" };
+  const initialState = {
+    loginStatus: null,
+    isAuth: false,
+    userId: null,
+    jwt: null
+  };
+  expect(authReducer(undefined, action)).toEqual(initialState);
+});
