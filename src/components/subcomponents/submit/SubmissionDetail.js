1b3f71cef30cc0a38a32370b4b9b3eb5b8e04237
Stanley <infamous_godhand@yahoo.com>
2020-04-21 13:14:17 +1000

    added icon to download button

--- src/components/subcomponents/submit/SubmissionDetail.js
+++ src/components/subcomponents/submit/SubmissionDetail.js
@@ -7,6 +7,7 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,7 +32,12 @@ const FileCard = (props) => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button
+          size="small"
+          color="primary"
+          variant="contained"
+          startIcon={<CloudDownloadIcon />}
+        >
           Download
         </Button>
       </CardActions>
