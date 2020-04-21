0b991897bbf7380c208c9edc7219bdad433c8d56
Stanley <infamous_godhand@yahoo.com>
2020-04-21 12:11:28 +1000

    some styling fixes

--- src/components/subcomponents/submit/SubmissionDetail.js
+++ src/components/subcomponents/submit/SubmissionDetail.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Typography, Paper } from "@material-ui/core";
+import { Typography, Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { getSubmissionData } from "../../../store/actions/hackathonActions";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,6 +16,29 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const FileCard = (props) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          content
+        </Typography>
+        <Typography color="textSecondary">adjective</Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
 const SubmissionDetail = ({ dispatch, submission }) => {
   const classes = useStyles();
   useEffect(() => {
@@ -31,9 +58,17 @@ const SubmissionDetail = ({ dispatch, submission }) => {
       <Typography style={{ textAlign: "center" }} variant="h4">
         Files
       </Typography>
-      {submission.files.map((file) => {
-        return <h3 key={file._id}>{file.name}</h3>;
-      })}
+      <Grid container spacing={3} className={classes.root}>
+        {submission.files
+          ? submission.files.map((file) => {
+              return (
+                <Grid key={file._id} xs={12} sm={3}>
+                  <FileCard />
+                </Grid>
+              );
+            })
+          : null}
+      </Grid>
     </Paper>
   );
 };
