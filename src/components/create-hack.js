a51770655b388269a8a4ef7aa2d4ee87bb2a004e
Stanley <infamous_godhand@yahoo.com>
2020-04-23 15:22:23 +1000

    start

--- src/components/create-hack.js
+++ src/components/create-hack.js
@@ -12,31 +12,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import Axios from "axios";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-
+import Status from "./subcomponents/status";
 import UrlJoin from "url-join";
 import { connect } from "react-redux";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     margin: theme.spacing(2),
     width: "80vw",
-    padding: theme.spacing(1)
+    padding: theme.spacing(1),
   },
   absolute: {
     position: "absolute",
     bottom: theme.spacing(2),
-    right: theme.spacing(3)
+    right: theme.spacing(3),
   },
   field: {
     padding: theme.spacing(1),
     textAlign: "left",
     color: theme.palette.text.secondary,
-    width: "90vw"
+    width: "90vw",
   },
   button: {
-    margin: theme.spacing(1)
-  }
+    margin: theme.spacing(1),
+  },
 }));
 
 export function CreateHack(props) {
@@ -47,7 +47,7 @@ export function CreateHack(props) {
   const [goal, setGoal] = useState("");
   const [join, setJoin] = useState(false);
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const fetchData = async () => {
       const newHack = {
@@ -55,15 +55,15 @@ export function CreateHack(props) {
         description: description,
         goal: goal,
         team: [],
-        creator: props.userId
+        creator: props.userId,
       };
       if (join) {
         newHack.team[0] = props.userId;
       }
       let config = {
         headers: {
-          Authorization: "Bearer " + props.token
-        }
+          Authorization: "Bearer " + props.token,
+        },
       };
       await Axios.post(
         UrlJoin(process.env.REACT_APP_API_URL, "addhack"),
@@ -105,7 +105,7 @@ export function CreateHack(props) {
                   <OutlinedInput
                     placeholder="Hack Name"
                     name="title"
-                    onChange={event => {
+                    onChange={(event) => {
                       setTitle(event.target.value);
                     }}
                     value={title}
@@ -121,7 +121,7 @@ export function CreateHack(props) {
                   <OutlinedInput
                     placeholder="Hack Description"
                     name="description"
-                    onChange={event => {
+                    onChange={(event) => {
                       setDescription(event.target.value);
                     }}
                     value={description}
@@ -139,7 +139,7 @@ export function CreateHack(props) {
                   <OutlinedInput
                     placeholder="Hack Goal"
                     name="goal"
-                    onChange={event => {
+                    onChange={(event) => {
                       setGoal(event.target.value);
                     }}
                     value={goal}
@@ -155,7 +155,7 @@ export function CreateHack(props) {
                     control={
                       <Checkbox
                         checked={join}
-                        onChange={event => {
+                        onChange={(event) => {
                           setJoin(event.target.checked);
                         }}
                         value="joinHack"
@@ -184,9 +184,9 @@ export function CreateHack(props) {
   );
 }
 
-const mapState = state => ({
+const mapState = (state) => ({
   token: state.auth.jwt,
-  userId: state.auth.userId
+  userId: state.auth.userId,
 });
 
 export default connect(mapState)(CreateHack);
